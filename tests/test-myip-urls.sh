f33efd4914ad3b2d2947809fc8685a9f7f8e194e
Artjom Slepnjov <shellgen@uncensored.citadel.org>
2025-12-04 11:58:33 +0000

    Update test-myip-urls script

--- tests/test-myip-urls.sh
+++ tests/test-myip-urls.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+# File: /bin/test-myip-urls
+# Desc: Checking requests for multiple myip sites from the list.
 # Usage: test-myip-urls /etc/getip-url.conf
 
 F=${1:?required: test-myip-urls </etc/getip-url.conf>}
 LOG="/tmp/test-urls.log"
 
-> "${LOG:?}"
+[ -w "${LOG}" ] && > "${LOG:?}"
+[ -x "www-get-ip.sh" ] && {
+	PATH="${PATH}${PATH:+:}./"
+	[ -x "www-get-ip" ] || ln -s www-get-ip.sh www-get-ip
+}
 
 N=0
 {
@@ -21,6 +27,6 @@ done < ${F}
 
 
 [ "${N}" -gt '0' ] &&
-printf '%s\n' "Success: ${N} request... ok"
+printf '%s\n' "Success: ${N} requests... ok"
 
 } 2>&1 | tee -a ${LOG}
