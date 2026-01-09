9cd01dc491f9b85745fa5b6c4e58ed6a79a7e1c7
Artjom Slepnjov <shellgen@uncensored.citadel.org>
2026-01-09 18:26:30 +0000

    Change and Update: test-myip-urls.sh

--- tests/test-myip-urls.sh
+++ tests/test-myip-urls.sh
@@ -2,6 +2,7 @@
 # File: /bin/test-myip-urls
 # Desc: Checking requests for multiple myip sites from the list.
 # Usage: test-myip-urls /etc/getip-url.conf
+# Example[file-rand-sort]: shuf ${file} > ${file}.new
 set -e -u -f
 
 IFS="$(printf '\n\t') "
@@ -16,15 +17,18 @@ LOG="/tmp/test-urls.log"
 	[ -x "www-get-ip" ] || ln -s www-get-ip.sh www-get-ip
 }
 
-N=0
+N=0; E=0
 {
 while IFS= read -r X; do
   X=${X%%#*}
   X="${X%${X##*[![:space:]]}}"
   if [ -n "${X}" ]; then
     printf '%s\n' "www-get-ip -g '${X}'"
-    www-get-ip -g "${X}"; N=$(expr "0${N}" + '1')
-    printf '\n'
+    ip=$(time -f %es www-get-ip -g "${X}") || { E=$(expr "0${E}" + '1'); continue;}
+    [ "${myip-}" ] || myip=${ip}
+    printf "${ip}\n\n"
+    [ x"${myip}" = x"${ip}" ] || { E=$(expr "0${E}" + '1'); printf "this ip is not your... error\n" >&2; continue;}
+    N=$(expr "0${N}" + '1')
     sleep 1
   fi
 done < ${F}
@@ -32,5 +36,8 @@ done < ${F}
 
 [ "${N}" -gt '0' ] &&
 printf '%s\n' "Success: ${N} requests... ok"
+
+[ "${E}" -gt '0' ] &&
+printf '%s\n' "Failed: ${E} requests... error"
 
 } 2>&1 | tee -a ${LOG}
